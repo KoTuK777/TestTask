778b5de07cf2fba7bc1674b763ed0a2ee097b7d3
Anton Menchinskii <58192865+KoTuK777@users.noreply.github.com>
2019-12-12 23:59:49 +0200

    Update TestTask.cpp

--- TestTask.cpp
+++ TestTask.cpp
@@ -16,12 +16,7 @@ private:
 
 public:
 	void setName(string newName) {
-		name = newName;
-	}
-
-	void setID() {
-		srand((unsigned)time(NULL));
-		if (id != 0) id = rand() % 100000 + 1;
+		if(name != "") name = newName;
 	}
 
 	void addRank() {
@@ -29,11 +24,13 @@ public:
 	}
 
 	void subRank() {
-		if(rank >= 25)	rank -= 25;
+		if (rank >= 25)	rank -= 25;
 	}
 
-	Player(string name) {
+	Player(string name, int rank, int id) {
 		setName(name);
+		this->rank = rank;
+		this->id = id;
 	}
 
 	string getName() {
@@ -45,39 +42,28 @@ public:
 	int getID() {
 		return id;
 	}
+	
 };
 
 class Hero {
 private:
-	string name = "";
-	int hp = -1;
-	int damage = -1;
-	int speed = -1;
+	string name;
+	int hp;
+	int damage;
+	int speed;
 
 public:
-	Hero(string n, int h, int d, int s) {
-		if (n != "") name = n;
-		if (h > 0) hp = h;
-		if (d > 0) damage = d;
-		if (s > 0) speed = s;
+	Hero(string name, int hp, int damage, int speed) {
+		if (name != "") this->name = name;
+		if (hp > 0) this->hp = hp;
+		if (damage > 0) this->damage = damage;
+		if (speed > 0) this->speed = speed;
 	}
-	/*void setName(string newName) {
-		if (name == "" && newName != "") name = newName;
-	}
-	void setHp(int newHp) {
-		if(hp == -1 && newHp > 0) hp = newHp;
-	}
-	void setDamage(int newDamage) {
-		if (damage == -1 && newDamage > 0) damage = newDamage;
-	}
-	void setSpeed(int newSpeed) {
-		if (speed == -1 && newSpeed > 0) speed = newSpeed;
-	}*/
 
 	string getName() {
 		return name;
 	}
-	int getHP(){
+	int getHP() {
 		return hp;
 
 	}
@@ -90,7 +76,7 @@ public:
 };
 
 class TeamPart {
-private: 
+private:
 	int playerId;
 	string heroName;
 
@@ -107,19 +93,29 @@ public:
 class Session {
 };
 
-
-
-
-
 int main() {
 
-	vector<void> players;
+	/*vector<void> players;
 	Player player("Tom");
 
 	players[0] = player;
 
-	cout << players[0] << endl;
+	cout << players[0] << endl;*/
 
+	Player player1("Tom", 1000, 0);
+	Player player2("Jame", 1000, 0);
+	Player player3("Stive", 1000, 0);
+
+
+	/*Player arr[] = {};*/
+
+
+
+	cout << player1.getName() << endl;
+	cout << player2.getName() << endl;
+	/*cout << arr[0].getName() << endl;*/
+
+	
 
 	return 0;
 	system("pause");
