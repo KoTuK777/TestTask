c570a868916b6b6b4fa144064338b5d7f5e416c4
Anton Menchinskii <58192865+KoTuK777@users.noreply.github.com>
2019-12-13 16:06:58 +0200

    Update TestTask.cpp

--- TestTask.cpp
+++ TestTask.cpp
@@ -18,7 +18,7 @@ private:
 
 public:
 	void setName(string newName) {
-		if(name != "") name = newName;
+		if (name != "") name = newName;
 	}
 
 	void addRank() {
@@ -49,7 +49,7 @@ public:
 	int getID() {
 		return id;
 	}
-	
+
 };
 
 class Hero {
@@ -60,6 +60,13 @@ private:
 	int speed;
 
 public:
+	Hero() {
+		name = "hero";
+		hp = 100;
+		damage = 5;
+		speed = 2;
+	}
+
 	Hero(string name, int hp, int damage, int speed) {
 		if (name != "") this->name = name;
 		if (hp > 0) this->hp = hp;
@@ -68,7 +75,7 @@ public:
 	}
 
 	string getName() {
-		cout << name << endl;
+		return name;
 	}
 	int getHP() {
 		return hp;
@@ -84,20 +91,35 @@ public:
 
 class TeamPart {
 private:
-	int playerId;
-	string heroName;
+	Player player;
+	Hero hero;
 
 public:
-	void setPlayer(int player) {
-		playerId = player;
-	}
-	void setHero(string hero) {
-		heroName = hero;
 
+
+	TeamPart() {
+		this->player.setName("unknown") ;
+	}
+	TeamPart(const Player &player, const Hero &hero) {
+		this->player = player;
+		this->hero = player;
 	}
 };
 
 class Session {
+	TeamPart teamRed[5];
+	TeamPart teamBlue[5];
+
+	/*void setRed(TeamPart* arr[]) {
+		for (int i = 0; i < 5; i++) {
+			teamBlue[i] = arr[i];
+		}
+	}*/
+
+	
+
+
+
 };
 
 
@@ -106,38 +128,72 @@ class Session {
 
 int main() {
 
+
+	string names[10] = {
+		"Tom", "Jame", "Stive", "John", "Rick", "Fred", "Eric", "Diego", "Luke", "Philip"
+	};
+	
+
 	/*vector<void> players;
 	Player player("Tom");
-
 	players[0] = player;
-
 	cout << players[0] << endl;*/
 	const int SIZE = 5;
+
+
 
 	Player player1("Tom", 1000);
 	Player player2("Jame", 1000);
 	Player player3("Stive", 1000);
 	Player player4("John", 1000);
 	Player player5("Rick", 1000);
+	Player player6("Philip", 1000);
+	Player player7("Luke", 1000);
+	Player player8("Diego", 1000);
+	Player player9("Eric", 1000);
+	Player player10("Fred", 1000);
 
+	Hero hero1();
+	Hero hero2();
+	Hero hero3();
+	Hero hero4();
+	Hero hero5();
+	Hero hero6();
+	Hero hero7();
+	Hero hero8();
+	Hero hero9();
+	Hero hero10();	
 
-	Player arr[SIZE];
+	TeamPart teampart1(player1, hero1);
+	TeamPart teampart2(Player player2, Hero hero2);
+	TeamPart teampart3(Player player3, Hero hero3);
+	TeamPart teampart4(Player player4, Hero hero4);
+	TeamPart teampart5(Player player5, Hero hero5);
+	TeamPart teampart6(Player player6, Hero hero6);
+	TeamPart teampart7(Player player7, Hero hero7);
+	TeamPart teampart8(Player player8, Hero hero8);
+	TeamPart teampart9(Player player9, Hero hero9);
+	TeamPart teampart10(Player player10, Hero hero10);
+
+	/*Player arr[SIZE];
 	arr[0] = player1;
 	arr[1] = player2;
 	arr[2] = player3;
+	arr[3] = player4;
+	arr[4] = player5;*/
+
+	
 
 
-
-
-	cout << arr[0].getID() << endl;
+	/*cout << arr[0].getID() << endl;
 	cout << arr[1].getID() << endl;
 	cout << arr[2].getID() << endl;
 	cout << player1.getID() << endl;
 	cout << player2.getID() << endl;
-	cout << player3.getID() << endl;
+	cout << player3.getID() << endl;*/
 	/*cout << arr[0].getName() << endl;*/
 
-	
+
 
 	return 0;
 	system("pause");
